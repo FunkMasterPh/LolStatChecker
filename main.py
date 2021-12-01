89a2acac4c46ec8b1ca016ecaefcfa6de80fa2ab
Philip Holmgren <philip.holmgren@live.com>
2021-12-01 21:34:35 +0100

    Added readme

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from champ_class import Champ
 from create_object import *
 from read_data import *
 from menu import *
-choice = input("Enter name of first champion you want the stats of: ")
+choice = input("Enter name of your champion: ")
 readData(choice)
 champ1 = createObj(choice)
-choice = input("Enter name of second champion you want the stats of: ")
+choice = input("Enter name of lane opponents champion: ")
 readData(choice)
 champ2 = createObj(choice)
 printMenu(champ1, champ2)
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Designed to compare stats between you and your lane opponent.
+
+Enter the names of your respective champions and compare the stats.
+
+The first letter in the champion name must be a capital letter
+If champion has several names (Miss Fortune, Aurelion Sol...) they are inputted as:
+MissFortune
+AurelionSol
